d5f66482075ad69bcde36644173eaab39ce08a12
himanshu <hkghimanshu@gmail.com>
2021-05-29 12:49:07 +0530

    made searchbar responsive

--- join-upipr-fe-master/src/pages/Home/index.js
+++ join-upipr-fe-master/src/pages/Home/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import logo from './star-wars-logo.png';
-import './index.css';
+import React, { useState } from "react";
+import logo from "./star-wars-logo.png";
+import { MdSearch } from "react-icons/md";
+
+import "./index.css";
 
 function HomePage() {
+  const [query, setQuery] = useState(" ");
+
   return (
     <div>
       <div className="logo">
         <img src={logo} alt="Star Wars Logo" />
       </div>
-      <input className="search-input" placeholder="Search by name" />
+      <div className="div-input">
+        <input className="search-input" placeholder="Search by name" />
+        <MdSearch className="search-icon" />
+      </div>
     </div>
   );
 }
