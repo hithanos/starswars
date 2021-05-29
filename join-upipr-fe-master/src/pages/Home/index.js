e3dfee177cd4427b40e1c758bef228212a27380b
himanshu <hkghimanshu@gmail.com>
2021-05-29 15:39:20 +0530

    added debouncing function

--- join-upipr-fe-master/src/pages/Home/index.js
+++ join-upipr-fe-master/src/pages/Home/index.js
@@ -1,11 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import logo from "./star-wars-logo.png";
 import { MdSearch } from "react-icons/md";
+import axios from "axios";
 
 import "./index.css";
 
 function HomePage() {
   const [query, setQuery] = useState(" ");
+  const [loading, setLoading] = useState(false);
+  const [suggestion, setSuggestion] = useState([]);
+
+  const characterFinder = (value) => {
+    console.log("API");
+    axios
+      .get("https://swapi.dev/api/people", {
+        params: {
+          q: value,
+        },
+      })
+      .then((res) => {
+        setSuggestion(res.data);
+        setLoading(true);
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err), setLoading(false));
+  };
+  const debouncing = function (fn, delay) {
+    let timerID;
+    return function () {
+      let self = this;
+      let args = arguments;
+      clearTimeout(timerID);
+      timerID = setTimeout(() => {
+        fn.apply(self, args);
+      }, delay);
+    };
+  };
+  const debounceData = useCallback(
+    debouncing((value) => characterFinder(value), 2000),
+    []
+  );
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setQuery(value);
+    if (value !== "") {
+      debounceData(value);
+    }
+  };
 
   return (
     <div>
@@ -13,7 +54,12 @@ function HomePage() {
         <img src={logo} alt="Star Wars Logo" />
       </div>
       <div className="div-input">
-        <input className="search-input" placeholder="Search by name" />
+        <input
+          className="search-input"
+          placeholder="Search by name"
+          value={query}
+          onChange={(e) => handleChange(e)}
+        />
         <MdSearch className="search-icon" />
       </div>
     </div>
